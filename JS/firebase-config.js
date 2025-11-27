b9e940b65c14c87bb3908962151cdeda3a156cc7
Raul Alvarez Villarraza <raul202@gmail.com>
2025-11-27 11:22:49 -0300

    firebase-config correccion

--- JS/firebase-config.js
+++ JS/firebase-config.js
@@ -1,10 +1,5 @@
   // Import the functions you need from the SDKs you need
 
-  import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-
-
-
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
